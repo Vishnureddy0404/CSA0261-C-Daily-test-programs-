8ff6ed6457a29e467b53f6575943d208f385156b
Vishnuvardhanreddy bandi <124245030+vishnureddy1729@users.noreply.github.com>
2023-03-30 09:24:24 +0530

    Add files via upload

---
+++ daily test1.2.29-03.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int  n,sum;
+	printf("enter a number:");
+	scanf("%d",n);
+	if(n<0,n,sum==0)
+		printf("sum of first n natural numbers,n");
+	else 
+	printf("sum of not first natural numbers,sum");
+	return 0;
+	}
